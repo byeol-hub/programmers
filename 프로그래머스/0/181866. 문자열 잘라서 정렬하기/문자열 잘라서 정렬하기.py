568066c64aa6c9a5b79471cf583bb74e6695c24c
byeol-hub <stell0705@naver.com>
2025-06-19 17:40:21 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 8.88 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,11 +1,23 @@
+# def solution(myString):
+#     answer = []
+#     arr = []
+#     arr = myString.split('x')
+#     for i in range(len(arr)):
+#         if arr[i] == "":
+#             continue
+#         else:
+#             answer.append(arr[i])
+#     answer.sort()
+#     return answer
+
 def solution(myString):
     answer = []
     arr = []
     arr = myString.split('x')
-    for i in range(len(arr)):
-        if arr[i] == "":
+    for i in arr:
+        if i == "":
             continue
         else:
-            answer.append(arr[i])
+            answer.append(i)
     answer.sort()
     return answer
