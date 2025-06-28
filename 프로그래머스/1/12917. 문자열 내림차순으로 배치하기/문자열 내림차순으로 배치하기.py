6797a65c8b19d9ea19af56a9325656637b0beda6
byeol-hub <stell0705@naver.com>
2025-06-28 11:37:12 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.09 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    arr = []
+    
+    for i in s:
+        arr.append(i)
+    
+    arr.sort(reverse = True)
+    
+    for i in arr:
+        answer += i
+    
+    return answer
