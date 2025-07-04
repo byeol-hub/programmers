46d2a9d88f0405e6ea9c93d939bdcb63bd7467eb
byeol-hub <stell0705@naver.com>
2025-07-04 16:36:59 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -3,9 +3,7 @@ def solution(chicken):
     c_chicken = 0
     
     answer += chicken // 10
-    print(answer)
     c_chicken = (chicken // 10) + (chicken % 10)
-    print(c_chicken)
     
     while (c_chicken // 10 != 0):
         answer += c_chicken // 10
