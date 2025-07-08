10dc2a42e96dfdc8d6cfa1a7222d8731221bdabe
byeol-hub <stell0705@naver.com>
2025-07-08 09:41:41 +0900

    [level 0] Title: 순서쌍의 개수, Time: 54.20 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    arr = []
+    
+    for i in range(1, n+1):
+        if n % i == 0:
+            arr.append(i)
+    
+    answer = len(arr)
+    
+    return answer
