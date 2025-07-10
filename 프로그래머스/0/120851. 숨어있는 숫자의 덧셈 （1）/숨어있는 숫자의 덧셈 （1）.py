34944d24c5678344d010eb113941a6b64b728516
byeol-hub <stell0705@naver.com>
2025-07-10 18:31:14 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 9.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = 0
+    num = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    
+    for i in my_string:
+        if i in num:
+            answer += int(i)
+    
+    return answer
+
