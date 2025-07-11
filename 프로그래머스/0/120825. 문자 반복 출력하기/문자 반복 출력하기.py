7d94848c97946528a6d9875de034446a11fbb664
byeol-hub <stell0705@naver.com>
2025-07-11 14:40:41 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,8 +1,16 @@
+# def solution(my_string, n):
+#     answer = ''
+    
+#     for i in range(len(my_string)):
+#         for j in range(n):
+#             answer += my_string[i]
+    
+#     return answer
+
 def solution(my_string, n):
     answer = ''
     
-    for i in range(len(my_string)):
-        for j in range(n):
-            answer += my_string[i]
+    for i in my_string:
+        answer += i*n
     
     return answer
