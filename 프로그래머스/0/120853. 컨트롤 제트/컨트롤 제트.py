976ba4ce53324ed4641c8c07939c919f969e8a90
byeol-hub <stell0705@naver.com>
2025-07-11 11:58:07 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 9.37 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = 0
+    arr = []
+    
+    arr = s.split(" ")
+    
+    for i in range(len(arr)):
+        if arr[i] == "Z":
+            answer -= int(arr[i-1])
+        else:
+            answer += int(arr[i])        
+    
+    return answer
