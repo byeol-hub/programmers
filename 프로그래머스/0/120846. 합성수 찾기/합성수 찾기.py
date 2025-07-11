523571480b38c179c9c52c70a98f09b18630e862
byeol-hub <stell0705@naver.com>
2025-07-11 14:45:11 +0900

    [level 0] Title: 합성수 찾기, Time: 0.12 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,16 +1,29 @@
+# def solution(n):
+#     answer = 0
+#     count = 0
+    
+#     if n >= 4:
+#         for i in range(4, n+1):
+#             for j in range(1, i+1):
+#                 if i % j == 0:
+#                     count += 1
+                
+#                 if count >= 3:
+#                     answer += 1
+#                     count = 0
+#                     break
+                    
+    
+#     return answer
+
 def solution(n):
     answer = 0
-    count = 0
     
     if n >= 4:
         for i in range(4, n+1):
-            for j in range(1, i+1):
+            for j in range(2, i):
                 if i % j == 0:
-                    count += 1
-                
-                if count >= 3:
                     answer += 1
-                    count = 0
                     break
                     
     
