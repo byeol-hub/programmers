d6b476d688a01a4013d20ccfd053bc0397d8dc5a
byeol-hub <stell0705@naver.com>
2025-07-11 15:55:58 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,15 +1,25 @@
+# def solution(sides):
+#     answer = 0
+    
+#     sides.sort()
+    
+#     for i in range(sides[1], sides[0] + sides[1]): # 가장 긴 변일 때
+#         answer += 1
+        
+#     for i in range(1, sides[1]): # 가장 긴 변이 아닐 때
+#         if i+sides[0] > sides[1]:
+#             answer += 1
+#         else:
+#             continue
+        
+#     return answer
+
+
 def solution(sides):
     answer = 0
     
     sides.sort()
     
-    for i in range(sides[1], sides[0] + sides[1]): # 가장 긴 변일 때
-        answer += 1
-        
-    for i in range(1, sides[1]): # 가장 긴 변이 아닐 때
-        if i+sides[0] > sides[1]:
-            answer += 1
-        else:
-            continue
-        
+    answer= sides[0] + abs(-sides[0] + 1)
+    
     return answer
