531c4d5d050f14d6a07feb24090f2ae10491091f
byeol-hub <stell0705@naver.com>
2025-07-12 12:29:08 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.11 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -0,0 +1,24 @@
+def solution(s):
+    answer = ''
+    s_unique = list(set(s))
+    s_count = []
+    answer_arr = []
+    
+    for i in range(len(s_unique)):
+        s_count.append(0)
+
+    for i in range(len(s_unique)):
+        for j in s:
+            if s_unique[i] == j:
+                s_count[i] += 1
+    
+    for i in range(len(s_count)):
+        if s_count[i] == 1:
+            answer_arr.append(s_unique[i])
+    
+    answer_arr.sort()
+    
+    for i in answer_arr:
+        answer += i
+    
+    return answer
