e82a057b956427490ef2370e195b060844583175
byeol-hub <stell0705@naver.com>
2025-07-17 23:43:05 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.06 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/README.md
+++ 프로그래머스/0/120871. 저주의 숫자 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 저주의 숫자 3 - 120871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120871#) 
 
 ### 성능 요약
 
-메모리: 9.33 MB, 시간: 0.22 ms
+메모리: 9.04 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 17일 23:40:45
+2025년 07월 17일 23:43:03
 
 ### 문제 설명
 
--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -3,13 +3,7 @@ def solution(n):
     
     for i in range(1, n+1):
         answer += 1
-        print("case1:",i, answer)
         while (answer % 3 == 0 or "3" in str(answer)):
-            if (answer//3 != 0 and answer % 3 == 0):
-                answer += 1
-                print("case2:",i, answer)
-            if answer % 3 != 0 and "3" in str(answer):
-                answer += 1
-                print("case3:",i, answer)
+            answer += 1
     
     return answer
