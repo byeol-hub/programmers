5f301120bae5b430f135e8162ead2afdda5b3d2f
byeol-hub <stell0705@naver.com>
2025-07-25 12:09:20 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,16 +1,29 @@
+# def solution(numbers, direction):
+#     answer = []
+    
+#     # right -> 맨 마지막 원소가 맨 처음으로 가고 나머지 원소들의 인덱스는 +1씩 이동
+#     # left -> 맨 처음 원소가 맨 마지막으로 가고 나머지 원소들의 인덱스는 -1씩 이동
+    
+#     if direction == "right":
+#         answer.append(numbers[-1])
+#         for i in numbers[:-1]:
+#             answer.append(i)
+#     else:
+#         for i in numbers[1:]:
+#             answer.append(i)
+#         answer.append(numbers[0])
+    
+#     return answer
+
+
 def solution(numbers, direction):
     answer = []
     
-    # right -> 맨 마지막 원소가 맨 처음으로 가고 나머지 원소들의 인덱스는 +1씩 이동
-    # left -> 맨 처음 원소가 맨 마지막으로 가고 나머지 원소들의 인덱스는 -1씩 이동
-    
     if direction == "right":
         answer.append(numbers[-1])
-        for i in numbers[:-1]:
-            answer.append(i)
+        answer += numbers[:-1]
     else:
-        for i in numbers[1:]:
-            answer.append(i)
+        answer = numbers[1:]
         answer.append(numbers[0])
     
     return answer
