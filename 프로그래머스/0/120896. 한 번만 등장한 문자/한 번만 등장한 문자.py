3d899a8c48acae0f594bb7a5ec1c1ba836578444
byeol-hub <stell0705@naver.com>
2025-07-25 12:54:36 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,24 +1,36 @@
+# def solution(s):
+#     answer = ''
+#     s_unique = list(set(s))
+#     s_count = []
+#     answer_arr = []
+    
+#     for i in range(len(s_unique)):
+#         s_count.append(0)
+
+#     for i in range(len(s_unique)):
+#         for j in s:
+#             if s_unique[i] == j:
+#                 s_count[i] += 1
+    
+#     for i in range(len(s_count)):
+#         if s_count[i] == 1:
+#             answer_arr.append(s_unique[i])
+    
+#     answer_arr.sort()
+    
+#     for i in answer_arr:
+#         answer += i
+    
+#     return answer
+
+
 def solution(s):
     answer = ''
-    s_unique = list(set(s))
-    s_count = []
-    answer_arr = []
+    s_set = list(set(s))
+    s_set.sort()
     
-    for i in range(len(s_unique)):
-        s_count.append(0)
-
-    for i in range(len(s_unique)):
-        for j in s:
-            if s_unique[i] == j:
-                s_count[i] += 1
-    
-    for i in range(len(s_count)):
-        if s_count[i] == 1:
-            answer_arr.append(s_unique[i])
-    
-    answer_arr.sort()
-    
-    for i in answer_arr:
-        answer += i
+    for i in s_set:
+        if s.count(i) == 1:
+            answer += i
     
     return answer
