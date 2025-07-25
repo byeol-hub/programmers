2d136003a2a379fed05c8b5270a73cdba8e95a16
byeol-hub <stell0705@naver.com>
2025-07-25 13:17:05 +0900

    [level 0] Title: 배열 만들기 2, Time: 89.55 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,32 +1,39 @@
-# def solution(l, r):
-#     answer = []
-#     for i in range(l,r):
-#         if (i % 5 == 0) and (str(i) in "5"):
-#             answer.append(i)
-#     if answer == []:
-#         answer.append(-1)
-#     return answer
-
-# def solution(l, r):
-#     answer = []
-#     for i in range(l,r):
-#         if i % 5 == 0:
-#             for j in str(i)[:-1]:
-#                 if j in "5":
-#                     answer.append(i)
-#     if answer == []:
-#         answer.append(-1)
-#     return answer
-
 def solution(l, r):
+    ans = []
     answer = []
+    array = ["0", "5"]
+    count = 0
+    
     for i in range(l,r+1):
-        if i % 5 == 0:
-            answer.append(i)
+        if (i % 5 == 0):
+            count = 0
             for j in str(i):
-                if j in ["1","2","3","4","6","7","8","9"]:
-                    answer.remove(i)
+                if j in array:
+                    count += 1
+                    continue
+                else:
                     break
-    if answer == []:
-        answer.append(-1)
-    return answer
+            if count == len(str(i)):
+                answer.append(str(i).replace('5','1'))
+                break
+                
+    for i in range(r, l-1, -1):
+        if (i % 5 == 0):
+            count = 0
+            for j in str(i):
+                if j in array:
+                    count += 1
+                    continue
+                else:
+                    break
+            if count == len(str(i)):                
+                answer.append(str(i).replace('5','1'))
+                break
+                
+    if answer != []:
+        for i in range(int(answer[0], 2), int(answer[1], 2) + 1):
+            ans.append(int(((bin(i))[2:]).replace('1','5')))
+    else:
+        ans.append(-1)
+    
+    return ans
