ea1bf9bab00b4ccfc5fd424622cb7f31cac3c1cc
byeol-hub <stell0705@naver.com>
2025-08-14 23:17:10 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    arr=[]
+    
+    for i in range(len(commands)):
+        arr = sorted(array[commands[i][0]-1:commands[i][1]])
+        answer.append(arr[commands[i][2]-1])
+    
+    return answer
