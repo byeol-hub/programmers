14acccbc11eb4136ba8eed65f5cb15b489291e3d
byeol-hub <stell0705@naver.com>
2025-08-16 13:01:31 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,11 +1,33 @@
 def solution(my_string):
     answer = []
-    num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     
-    for i in my_string:
-        if i in num:
-            answer.append(int(i))
+    for i in range(len(my_string)):
+        if my_string[i] < 'a' or my_string[i] > 'z':
+            answer.append(int(my_string[i]))
     
     answer.sort()
     
     return answer
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(my_string):
+#     answer = []
+#     num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    
+#     for i in my_string:
+#         if i in num:
+#             answer.append(int(i))
+    
+#     answer.sort()
+    
+#     return answer
