5a58440c00482ea6110073f93e316937ff344f2f
byeol-hub <stell0705@naver.com>
2025-08-16 13:07:32 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,11 +1,38 @@
 def solution(emergency):
     answer = []
-    answer = emergency.copy()
-    arr = emergency.copy()
+    emergency_sort = []
     
-    arr.sort(reverse=True)
+    emergency_sort = sorted(emergency, reverse=True)
     
     for i in range(len(emergency)):
-        answer[i] = arr.index(emergency[i]) + 1
+        answer.append(emergency_sort.index(emergency[i])+1)
     
     return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(emergency):
+#     answer = []
+#     answer = emergency.copy()
+#     arr = emergency.copy()
+    
+#     arr.sort(reverse=True)
+    
+#     for i in range(len(emergency)):
+#         answer[i] = arr.index(emergency[i]) + 1
+    
+#     return answer
